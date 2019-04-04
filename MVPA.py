3069aee36d97697fabb5f35b21c26798d10a0c2b
Stijn Nuiten <stijnnuiten@gmail.com>
2019-04-04 11:59:02 +0200

    added comments

--- MVPA.py
+++ MVPA.py
@@ -65,47 +65,73 @@ class MVPA(object):
 		self.event_ids = event_ids
 		print self.ID
 
+		# find epoch file and load epochs
 		self.epochFilename = glob.glob(os.path.join(self.subDir, '*' + str(self.index) + '*_epo.fif'))[-1]
 		self.epochs =  mne.read_epochs(self.epochFilename, preload=True)
+
+		# load trial-parameter file
 		self.params = pd.read_csv(os.path.join(self.subDir, self.subject + '_' + str(self.index) + '_params.csv'))
 
-		self.selection = selection
+		# Channel selection pre-sets
 		self.chanSel = {}
 		self.chanSel['ALL'] = self.epochs.info['ch_names'][0:64]
 		self.chanSel['OCC'] = ['Oz','O1','O2', 'PO7', 'PO3', 'POz', 'PO4', 'PO8', 'Iz']
+		self.chanSel['PAR'] = ['P1', 'P3', 'P5', 'P7', 'Pz', 'P2', 'P4', 'P6', 'P8']
+		self.chanSel['FRO'] = ['Fp1', 'AF7', 'AF3', 'Fpz', 'Fp2', 'AF8', 'AF4', 'AFz', 'Fz']
+		self.chanSel['TMP'] = ['FT7', 'C5', 'T7', 'TP7', 'CP5', 'FT8', 'C6', 'T8', 'TP8', 'CP6']
+		self.chanSel['OPA'] = ['P1', 'P3', 'P5', 'P7', 'P9', 'Pz', 'P2', 'P4', 'P6', 'P8', 'P10', 'PO7', 'PO3'
+		  					   'O1', 'Iz', 'Oz', 'POz', 'PO8', 'PO4', 'O2', 'PO9', 'PO10' ]
+		self.chanSel['CDA'] = ['P5', 'P6', 'P7', 'P8', 'PO7', 'PO8', 'O1', 'O2', 'PO9', 'PO10'];
+
+		# Pick the desired channels
 		self.epochs.pick_channels(self.chanSel[selection])
 
+		# split up all epochs according to stimulus class
 		self.epochsClass1 = self.epochs[event_ids.keys()[0]]
 		self.epochsClass2 = self.epochs[event_ids.keys()[1]]
  
+ 		# create output directory, if necessaru
 		self.decDir = os.path.join(self.subDir + 'decoding/')
 		if not os.path.isdir(self.decDir):
 			os.mkdir(self.decDir)
 
 	def SVM(self, method, times=[-0.2, 1.0], decim=8, supra=False):
+		# This method trains a support vector machine on 90% of the trials to separate conditions 
+		# (2 as of now), and then tests this model on the remaining 10%. This is done in a 10-fold procedure.
+		# Right now, only this procedure -and calculating auc values- are included.
 
+		# crop data if desired
 		self.epochsClass1.crop(times[0],times[1])
 		self.epochsClass2.crop(times[0],times[1])
+
+		# decimate the signal to speed up, if desired
 		self.epochsClass1 =  self.epochsClass1.decimate(decim)
 		self.epochsClass2 =  self.epochsClass2.decimate(decim)
 
+		# determine condition names
 		conds = self.event_ids.keys()
 		condname = conds[0] + ' vs ' + conds[1]
 		if '/' in condname:
-			condname = condname.replace('/','-')
+			condname = condname.replace('/','-') # replace '/' with '-' to prevent creating subfolders during saving of data/figures
 
+		# give epochs labels (0: cond1, 1: cond2)
 		y = np.hstack([np.zeros(len(self.epochsClass1),dtype='bool'), np.ones(len(self.epochsClass2),dtype ='bool')])
+		# concatenate data belonging to both conds
 		X = np.concatenate([self.epochsClass1.get_data(), self.epochsClass2.get_data()])
 
+		# initiate the SVM
 		svc = SVC(C=1, kernel='linear')
 		clf = make_pipeline(StandardScaler(), svc)
 		cv  = ShuffleSplit(n_splits=10, test_size=0.1)
+
+		# time_decod is for diagonal decoding, time_gen for temporal generalization (incl. off-diagonal)
 		time_decod = SlidingEstimator(clf, n_jobs=1, scoring='roc_auc')
 		time_gen = GeneralizingEstimator(clf, n_jobs=1, scoring='roc_auc')
 
-		chance = 1.0/len(conds)
+		# Determine chance level
+		chance = 1.0/len(conds) 
 
-		if supra:
+		if supra: # average over X-trials (now only 4), to reduce noise and increase decoding accuracy, if desired
 			# Find data belonging to present vs absent
 			Class1TrialDat = X[~y,:,:]
 			Class2TrialDat  = X[y,:,:]
@@ -129,9 +155,11 @@ class MVPA(object):
 	
 		scoresAll = scores
 
+		# Calculate mean and SEM for plotting
 		scores = np.mean(scores, axis=0)
 		scoresSEM = np.std(scoresAll,axis=0)/np.sqrt(scoresAll.shape[0])
 
+		# store averaged scores in dataFrame and save
 		df = pd.DataFrame(data=scores)
 		df.insert(0,'times',self.epochsClass1.times) 
 		df.to_csv(os.path.join(self.decDir, self.subject + '_' + str(self.index) + '_SVM_' + method + '_' + condname + '_' + self.selection + (' supra' * supra) +'.csv'),index=False)
@@ -152,7 +180,7 @@ class MVPA(object):
 			ax.axhline(0, color='k')
 			plt.colorbar(im, ax=ax)
 
-			plt.savefig(fname = os.path.join(plotDir, self.subject + '_' + str(self.index) + '_tempGen ' +  condname + '_' + self.selection + (' supra' * supra) + '.pdf'),format= 'pdf')
+			plt.savefig(fname = os.path.join(plotDir, self.subject + '_' + str(self.index) + '_SVM_' + method +  condname + '_' + self.selection + (' supra' * supra) + '.pdf'),format= 'pdf')
 			plt.close()
 
 		elif method == 'diag':		
@@ -165,10 +193,12 @@ class MVPA(object):
 			ax.legend()
 			ax.axvline(0., color='k', linestyle='--')
 			ax.set_title('Sensor space decoding ' + condname + ' ' + self.task)
-			plt.savefig(fname = os.path.join(plotDir, self.subject + '_' +  str(self.index) + '_SVM ' + condname + '_' + self.selection + (' supra' * supra) + '.pdf'),format= 'pdf')
+			plt.savefig(fname = os.path.join(plotDir, self.subject + '_' +  str(self.index) + '_SVM_' + method + '_'+ condname + '_' + self.selection + (' supra' * supra) + '.pdf'),format= 'pdf')
 			plt.close()
 
 	def groupLevel(self, subs, idx, task, method, cond ='*', supra = False, selection = 'ALL',**kwargs):	
+		
+		# Load in data
 		Mat = np.array([])
 		for s in subs:
 			classDir = self.baseDir + task + '/' + s + '/decoding/'
@@ -181,6 +211,7 @@ class MVPA(object):
 			elif method == 'tempGen':
 				Mat = np.append(Mat,dat.values[:,1:])	
 
+		# reshape to get subject x data (time/timextime) array
 		if method == 'diag':
 			allDat = Mat.reshape(len(subs),dat.shape[0])
 		elif method == 'tempGen':
@@ -189,14 +220,18 @@ class MVPA(object):
 		cond = filename.split('/')[-1].split('_')[-2] if cond == '*' else cond
 
 		chance = np.ones(allDat.shape)*0.5
+
+		# calculate mean and SEM over subjects
 		allDatMean = allDat.mean(axis=0)
 		allDatSEM = np.std(allDat,axis=0)/np.sqrt(allDat.shape[0])
 		
-		# times = linspace(-200,1200,allDat.shape[1])
+		# find index where time = 0
 		tzero = (np.abs(0-times)).argmin()
 		timelabels = list(linspace(tzero,len(times)-1,5,dtype=int))
 
 		if method == 'diag':
+			# calculate cluster-corrected pvals, under the majority null-hypothesis (see prevInference function)
+			# And the global null hypothesis (which is theoretically problematic)
 			pvals, pvalsGN = prevInference(data=allDat)
 
 			plt.suptitle('Mean decoding performance (N=%i). %s %s' %(allDat.shape[0], selection + ' channels', ' supra' * supra ))
@@ -207,11 +242,16 @@ class MVPA(object):
 				
 			plt.axhline(0.5, color='k',linestyle='-.')
 			plt.axvline(0., color='k')
+
+			# h3 = plt.plot(linspace(-200,1200,allDat.shape[1]),(pvals<0.05)*0.45 , 'ro', label='Majority Null')
+
 			h4 = plt.plot(linspace(times[0],times[-1],allDat.shape[1]),(pvalsGN<0.05)*0.45 , 'go', label='Global Null')
 			plt.legend(handles=[ h4], labels=['Global Null'])
 
 		elif method == 'tempGen':
+			# run cluster-corrected t-test (global null hypothesis only)
 			t_thresh = cluster_ttest(chance,allDat,1000, 0.05)
+			
 			x = np.linspace(0,t_thresh.shape[1], t_thresh.shape[1]*100)
 			y = np.linspace(0,t_thresh.shape[0], t_thresh.shape[0]*100)
 			plt.imshow(allDatMean,cmap = 'RdBu_r',origin = 'lower',vmin=0, vmax = 1)
